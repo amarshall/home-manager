060612ed907c4c9082d25ec99b5576694dc2fd8b
Andrew Marshall <andrew@johnandrewmarshall.com>
2023-03-01 21:26:39 -0500

    home: Add buildEnvWithNoChroot to help avoid darwin sandbox failures
    Sets `__noChroot = true` on select `buildEnv` derivations that assemble
    large numbers of paths. This may be used to avoid sandbox failures on
    darwin, see https://github.com/NixOS/nix/issues/4119 and the `sandbox`
    option in `man nix.conf`.

--- modules/home-environment.nix
+++ modules/home-environment.nix
@@ -458,6 +458,14 @@ in
         will be printed when the user configuration is being built.
       '';
     };
+
+    home.buildEnvWithNoChroot = mkEnableOption ''
+      Sets <code>__noChroot = true</code> on select <code>buildEnv</code>
+      derivations that assemble large numbers of paths, as well the activation
+      script derivations. This may be used to avoid sandbox failures on Darwin,
+      see https://github.com/NixOS/nix/issues/4119 and the <code>sandbox</code>
+      option in <command>man nix.conf</command>.
+    '';
   };
 
   config = {
@@ -693,7 +701,7 @@ in
         )
         + optionalString (!cfg.emptyActivationPath) "\${PATH:+:}$PATH";
 
-        activationScript = pkgs.writeShellScript "activation-script" ''
+        activationScript = (pkgs.writeShellScript "activation-script" ''
           set -eu
           set -o pipefail
 
@@ -705,9 +713,11 @@ in
           ${builtins.readFile ./lib-bash/activation-init.sh}
 
           ${activationCmds}
-        '';
+        '').overrideAttrs (old: {
+          __noChroot = cfg.buildEnvWithNoChroot;
+        });
       in
-        pkgs.runCommand
+        (pkgs.runCommand
           "home-manager-generation"
           {
             preferLocalBuild = true;
@@ -729,9 +739,11 @@ in
             ln -s ${cfg.path} $out/home-path
 
             ${cfg.extraBuilderCommands}
-          '';
+          '').overrideAttrs (old: {
+            __noChroot = cfg.buildEnvWithNoChroot;
+          });
 
-    home.path = pkgs.buildEnv {
+    home.path = (pkgs.buildEnv {
       name = "home-manager-path";
 
       paths = cfg.packages;
@@ -742,6 +754,8 @@ in
       meta = {
         description = "Environment of packages installed through home-manager";
       };
-    };
+    }).overrideAttrs (old: {
+      __noChroot = cfg.buildEnvWithNoChroot;
+    });
   };
 }
